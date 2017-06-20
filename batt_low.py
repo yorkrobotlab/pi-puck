e380990c07e7b553fabdf1e12a7053e4a9c3a4fe
Russell Joyce <russell.joyce@york.ac.uk>
2017-06-20 16:46:16 +0100

    Added message broadcasts for shutdown scripts Notifies all logged in terminals when action is triggered or cancelled

--- batt_low.py
+++ batt_low.py
@@ -15,14 +15,17 @@ while True:
 	# Wait indefinitely for a falling edge
 	GPIO.wait_for_edge(pin, GPIO.FALLING)
 	print("BATT_LOW shutdown request received")
+	os.system("wall 'BATT_LOW signal asserted. Powering off in 2 seconds...'")
 	
 	# Wait for up to 2 seconds for a rising edge (timeout is in milliseconds)
 	result = GPIO.wait_for_edge(pin, GPIO.RISING, timeout=2000)
 	
 	if result is None and not GPIO.input(pin):
 		print('BATT_LOW still asserted, time to shutdown...')
+		os.system("wall 'BATT_LOW signal still asserted. Powering off now...'")
 		os.system("shutdown -h now")
 	else:
 		print('BATT_LOW signal changed, shutdown cancelled.')
+		os.system("wall 'BATT_LOW signal de-asserted. Power off cancelled.'")
 	
 	time.sleep(0.1)
--- gpio_shutdown.py
+++ gpio_shutdown.py
@@ -15,14 +15,17 @@ while True:
 	# Wait indefinitely for a rising edge
 	GPIO.wait_for_edge(pin, GPIO.RISING)
 	print("Switch turned on, waiting for timeout...")
+	os.system("wall 'Shutdown switch turned on. Powering off in 5 seconds...'")
 	
 	# Wait for up to 5 seconds for a falling edge (timeout is in milliseconds)
 	result = GPIO.wait_for_edge(pin, GPIO.FALLING, timeout=5000)
 	
 	if result is None and GPIO.input(pin):
 		print('Switch is still on, time to shutdown...')
+		os.system("wall 'Shutdown switch still on. Powering off now...'")
 		os.system("shutdown -h now")
 	else:
 		print('Switch turned off, shutdown cancelled.')
+		os.system("wall 'Shutdown switch turned off. Power off cancelled.'")
 	
 	time.sleep(0.1)
