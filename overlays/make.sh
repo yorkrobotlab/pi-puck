71f82054b0bc18df8ec59edbd2ee6dc5f036093c
Russell Joyce <russell.joyce@york.ac.uk>
2019-03-11 13:58:19 +0000

    Added overlay for first production board prototype
    Also updated make script to build all overlays.

--- overlays/make.sh
+++ overlays/make.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-../../linux/scripts/dtc/dtc -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-bottom.dtbo pi-puck-v3_0-bottom.dts
-../../linux/scripts/dtc/dtc -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-top.dtbo pi-puck-v3_0-top.dts
-../../linux/scripts/dtc/dtc -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-switchboard.dtbo pi-puck-v3_0-switchboard.dts
-../../linux/scripts/dtc/dtc -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-sensorboards.dtbo pi-puck-v3_0-sensorboards.dts
+DTC=../../linux/scripts/dtc/dtc
+
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v1_0.dtbo pi-puck-v1_0.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v2_0.dtbo pi-puck-v2_0.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0_proto.dtbo pi-puck-v3_0_proto.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-bottom.dtbo pi-puck-v3_0-bottom.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-top.dtbo pi-puck-v3_0-top.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-switchboard.dtbo pi-puck-v3_0-switchboard.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_0-sensorboards.dtbo pi-puck-v3_0-sensorboards.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v3_1_cameratestboard.dtbo pi-puck-v3_1_cameratestboard.dts
+$DTC -@ -I dts -O dtb -W no-unit_address_vs_reg -o pi-puck-v4_0.dtbo pi-puck-v4_0.dts
